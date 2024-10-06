c551f570c3899981aa4bb42243ea308834cc7622
JonathanGRiego02 <jonathangrclases@gmail.com>
2024-10-06 17:12:41 +0100

    Proyecto iniciado y fxml

---
+++ Calculadora/src/main/java/org/dad/CalculadoraApp.java
@@ -0,0 +1,14 @@
+package org.dad;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CalculadoraApp extends Application {
+
+  @Override
+  public void start(Stage primayStage) throws Exception{
+
+  }
+
+}
