7cb51bbab594975a30be98ebac06b7705954aa53
JonathanGRiego02 <jonathangrclases@gmail.com>
2024-10-08 12:14:47 +0100

    controlador empezado

--- Calculadora/src/main/java/org/dad/CalculadoraController.java
+++ Calculadora/src/main/java/org/dad/CalculadoraController.java
@@ -1,11 +1,104 @@
 package org.dad;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CalculadoraController implements Initializable {
+
+  // View
+
+  @FXML
+  private Button cButton;
+
+  @FXML
+  private Button ceButton;
+
+  @FXML
+  private Button divButton;
+
+  @FXML
+  private Button mulButton;
+
+  @FXML
+  private Button igualButton;
+
+  @FXML
+  private Button restaButton;
+
+  @FXML
+  private Button ceroButton;
+
+
+  @FXML
+  private Button unoButton;
+
+  @FXML
+  private Button dosButton;
+
+  @FXML
+  private Button tresButton;
+
+  @FXML
+  private Button cuatroButton;
+
+  @FXML
+  private Button cincoButton;
+
+  @FXML
+  private Button seisButton;
+
+  @FXML
+  private Button sieteButton;
+
+  @FXML
+  private Button ochoButton;
+
+  @FXML
+  private Button nueveButton;
+
+  @FXML
+  private TextField pantallaTextField;
+
+  @FXML
+  private Button puntoButton;
+
+  @FXML
+  private GridPane root;
+
+  @FXML
+  private Button sumButton;
+
+  @FXML
+  void onBorrarAction(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onBorrarTodoAction(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onComaAction(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onDigitAction(ActionEvent event) {
+
+  }
+
+  @FXML
+  void onOperarAction(ActionEvent event) {
+
+  }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
